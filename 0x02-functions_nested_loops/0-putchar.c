3ce47861a3c3d2810afde3831dae6545eb7d685b
opeyemi2001 <olokoopeyemi06@gmail.com>
2022-03-15 08:20:26 -0600

    Committed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,13 @@
-#include <main.h>
-int main()
+#include "main.h"
+int main(void)
 {
-	printf("_putchar");
-	printf("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return 0;
 }
